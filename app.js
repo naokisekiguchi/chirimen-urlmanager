90410baaf71d833084f8856d83f792e7899b2d64
naokisekiguchi <na-sekiguchi@kddilabs.jp>
2017-09-30 18:11:30 +0900

    prototype

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
 window.addEventListener("load", function() {
-  console.log("Hello World!");
+  const urlmanager = new UrlManager({
+    domain:"http://192.168.212.21:3000/api/",
+    id:"meidai",
+  });
+
+  setInterval(urlmanager.polling.bind(urlmanager),1000);
 });
+
+function UrlManager(configs){
+  this.domain = configs.domain;
+  this.id = configs.id;
+  this.lastupdated = null;
+  this.frame = document.getElementById("myframe");
+}
+
+UrlManager.prototype = {
+  polling:function(){
+    let self = this;
+    let xhr = new XMLHttpRequest({mozSystem: true});
+    let url = this.domain+this.id;
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function(){
+      // 本番用
+      if (xhr.readyState === 4 && xhr.status === 200){
+        console.log(xhr.responseText);
+        self.openUrl(JSON.parse(xhr.responseText)[0]);
+      }
+    };
+    xhr.send(null);
+  },
+  openUrl:function(json){
+    let url = json.url;
+    if(this.lastupdated != json.lastupdated){
+      this.lastupdated = json.lastupdated;
+      this.frame.src = url;
+    }
+  }
+}
